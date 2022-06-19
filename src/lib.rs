077bf90b962376b6fcb1a17b4d9dc5680bdd715b
S.Percentage <Syn.Tri.Naga@gmail.com>
2022-06-19 12:33:33 +0900

    init: helper utils

---
+++ src/lib.rs
@@ -0,0 +1,45 @@
+
+/// An marker for FFI safe object with objective-c
+pub unsafe trait ObjcObject {
+    fn as_id(&self) -> &objc::runtime::Object;
+    fn as_id_mut(&mut self) -> &mut objc::runtime::Object;
+}
+
+#[macro_export]
+macro_rules! DefineObjcObjectWrapper {
+    ($v: vis $tyname: ident) => {
+        #[repr(C)]
+        $v struct $tyname(objc::runtime::Object);
+        unsafe impl $crate::ObjcObject for $tyname {
+            fn as_id(&self) -> &objc::runtime::Object { &self.0 }
+            fn as_id_mut(&mut self) -> &mut objc::runtime::Object { &mut self.0 }
+        }
+    };
+    ($v: vis $tyname: ident : $super: ty) => {
+        $crate::DefineObjcObjectWrapper!($v $tyname);
+        impl std::ops::Deref for $tyname {
+            type Target = $super;
+            fn deref(&self) -> &Self::Target {
+                unsafe { std::mem::transmute(self) }
+            }
+        }
+        impl std::ops::DerefMut for $tyname {
+            fn deref_mut(&mut self) -> &mut Self::Target {
+                unsafe { std::mem::transmute(self) }
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! IdObject {
+    ($v: vis $name: ident = id < $($p: path),+ >) => {
+        #[repr(C)]
+        $v struct $name(objc::runtime::Object);
+        unsafe impl $crate::ObjcObject for $name {
+            fn as_id(&self) -> &objc::runtime::Object { &self.0 }
+            fn as_id_mut(&mut self) -> &mut objc::runtime::Object { &mut self.0 }
+        }
+        $(unsafe impl $p for $name {})+
+    }
+}
