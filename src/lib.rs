d89b2a61bc0b791dce2e20441331609db47d2373
S.Percentage <2141528+Pctg-x8@users.noreply.github.com>
2024-12-31 21:02:20 +0900

    fix: add some trait impls for objc object wrapper

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,12 @@ pub unsafe trait ObjcObject {
     fn as_id_mut(&mut self) -> &mut objc::runtime::Object;
 }
 unsafe impl ObjcObject for objc::runtime::Object {
+    #[inline(always)]
     fn as_id(&self) -> &objc::runtime::Object {
         self
     }
+
+    #[inline(always)]
     fn as_id_mut(&mut self) -> &mut objc::runtime::Object {
         self
     }
@@ -15,9 +18,12 @@ unsafe impl<T> ObjcObject for &'_ mut T
 where
     T: ObjcObject,
 {
+    #[inline(always)]
     fn as_id(&self) -> &objc::runtime::Object {
         T::as_id(self)
     }
+
+    #[inline(always)]
     fn as_id_mut(&mut self) -> &mut objc::runtime::Object {
         T::as_id_mut(self)
     }
@@ -25,44 +31,89 @@ where
 
 #[macro_export]
 macro_rules! DefineObjcObjectWrapper {
-    ($v: vis $tyname: ident) => {
-        #[repr(C)]
+    ($(#[$a: meta])* $v: vis $tyname: ident : $super: ty) => {
+        #[repr(transparent)]
+        $(#[$a])*
         $v struct $tyname(objc::runtime::Object);
-        $crate::DefineObjcObjectWrapper!(ext_struct $tyname);
+        $crate::DefineObjcObjectWrapper!(__ext_struct $tyname : $super);
     };
-    ($v: vis $tyname: ident : $super: ty) => {
-        #[repr(C)]
+    ($(#[$a: meta])* $v: vis $tyname: ident) => {
+        #[repr(transparent)]
+        $(#[$a])*
         $v struct $tyname(objc::runtime::Object);
-        $crate::DefineObjcObjectWrapper!(ext_struct $tyname : $super);
+        $crate::DefineObjcObjectWrapper!(__ext_struct $tyname);
     };
 
-    (ext_struct $tyname: ident) => {
+    { $(#[$a: meta])* $v: vis $tyname: ident : $super: ty; } => {
+        #[repr(transparent)]
+        $(#[$a])*
+        $v struct $tyname(objc::runtime::Object);
+        $crate::DefineObjcObjectWrapper!(__ext_struct $tyname : $super);
+    };
+    { $(#[$a: meta])* $v: vis $tyname: ident; } => {
+        #[repr(transparent)]
+        $(#[$a])*
+        $v struct $tyname(objc::runtime::Object);
+        $crate::DefineObjcObjectWrapper!(__ext_struct $tyname);
+    };
+
+    (__ext_struct $tyname: ident) => {
         unsafe impl $crate::ObjcObject for $tyname {
+            #[inline(always)]
             fn as_id(&self) -> &objc::runtime::Object { &self.0 }
+            #[inline(always)]
             fn as_id_mut(&mut self) -> &mut objc::runtime::Object { &mut self.0 }
         }
+
         unsafe impl objc::Encode for &'_ $tyname {
+            #[inline(always)]
             fn encode() -> objc::Encoding {
                 <&objc::runtime::Object as objc::Encode>::encode()
             }
         }
         unsafe impl objc::Encode for &'_ mut $tyname {
+            #[inline(always)]
             fn encode() -> objc::Encoding {
                 <&mut objc::runtime::Object as objc::Encode>::encode()
             }
         }
+
+        unsafe impl objc::Message for $tyname {
+            #[inline(always)]
+            unsafe fn send_message<A, R>(&self, sel: objc::runtime::Sel, args: A)
+                        -> Result<R, objc::MessageError>
+                        where Self: Sized, A: objc::MessageArguments, R: std::any::Any {
+                self.0.send_message(sel, args)
+            }
+        
+            #[inline(always)]
+            fn verify_message<A, R>(&self, sel: objc::runtime::Sel) -> Result<(), objc::MessageError>
+                        where Self: Sized, A: objc::EncodeArguments, R: objc::Encode {
+                self.0.verify_message(sel)
+            }
+        }
+        impl objc::MessageArguments for $tyname {
+            #[inline(always)]
+            unsafe fn invoke<R>(imp: objc::runtime::Imp, obj: *mut objc::runtime::Object, sel: objc::runtime::Sel, args: Self) -> R
+                        where R: std::any::Any {
+                <objc::runtime::Object as objc::MessageArguments>::invoke(imp, obj, sel, args.0)
+            }
+        }
     };
-    (ext_struct $tyname: ident : $super: ty) => {
-        $crate::DefineObjcObjectWrapper!(ext_struct $tyname);
+    (__ext_struct $tyname: ident : $super: ty) => {
+        $crate::DefineObjcObjectWrapper!(__ext_struct $tyname);
         impl std::ops::Deref for $tyname {
             type Target = $super;
+
+            #[inline(always)]
             fn deref(&self) -> &Self::Target {
-                unsafe { std::mem::transmute(self) }
+                unsafe { core::mem::transmute(self) }
             }
         }
         impl std::ops::DerefMut for $tyname {
+            #[inline(always)]
             fn deref_mut(&mut self) -> &mut Self::Target {
-                unsafe { std::mem::transmute(self) }
+                unsafe { core::mem::transmute(self) }
             }
         }
     }
